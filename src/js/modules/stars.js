e1056e192e280e09f3cb2588a9b847bf8360ed63
aleksei.kazulin <Kazulin.job@gmail.com>
2022-03-21 18:56:51 +0300

    page survey: stars scripts; scss background url fix; header search fix

--- src/js/modules/stars.js
+++ src/js/modules/stars.js
@@ -1,13 +1,29 @@
 export const getStars = () => {
-    if(document.querySelector('.js-starsCount')) {
+    function starsResult() {
         let starsBlock = document.querySelectorAll('.js-starsWrap');
         starsBlock.forEach(function(elem) {
             let starsResult = elem.querySelectorAll('span');
-            let starsCount = document.querySelector('.js-starsCount').getAttribute('data-count');
+            let starsCount = elem.querySelector('.js-starsCount').getAttribute('data-count');
             for (let i = 0; i < starsCount; i++) {
                 starsResult[i].classList.add('active');
             }
-            console.log(starsCount)
         })
+    }
+    if(document.querySelector('.js-starsCount')) {
+        starsResult();
+    }
+    if(document.querySelector('.js-surveyForm')) {
+        let surveyForm = document.querySelector('.js-surveyForm');
+        surveyForm.addEventListener("click", function (e) {
+            if (e.target.getAttribute('type') == 'radio') {
+                let starsParent = e.target.closest('.js-ratingInput');
+                let starsValue = (starsParent.querySelector('input[type="radio"]:checked').value);
+                let surveyQuestion = e.target.closest('.js-surveyQuestion');
+                let QuestionNumber = surveyQuestion.getAttribute('data-survey');
+                let resultBlock = document.querySelector(`[data-result="${QuestionNumber}"]`)
+                resultBlock.setAttribute('data-count', starsValue);
+                starsResult();
+            }
+        });
     }
 }
